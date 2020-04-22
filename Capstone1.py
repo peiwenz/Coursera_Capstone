79951298fdaab3a43ef2c1616fb55c90a7295c64
peiwenz <49793084+peiwenz@users.noreply.github.com>
2020-04-22 14:45:27 -0400

    Add files via upload

---
+++ Capstone1.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# **This file is used for the Coursera IBM Data Science Certificate: Capstone**
+
+# In[1]:
+
+import numpy as np
+import pandas as pd
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
